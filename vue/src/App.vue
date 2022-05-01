07725e0e2580ce9b4a1d4194dba9a8ec94e3cc27
Qais Wardag <47112831+qaiswardag@users.noreply.github.com>
2022-05-01 20:01:55 +0200

    Add store, router and tailwindcss to vue

---
+++ vue/src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import {mapState} from 'vuex'
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {HelloWorld},
+  computed: {
+    ...mapState(['user'])
+  },
+
+
+}
+
+</script>
+
+<template>
+  <div>
+    <div class="text-center border p-8">
+      example 1: {{ user }}
+    </div>
+
+    <HelloWorld msg="this is a prop"></HelloWorld>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/src/store/index.js
@@ -0,0 +1,17 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+  state: {
+    user: {
+      data: {name: 'zura'},
+      token: null
+    }
+  },
+  getters: {},
+  actions: {},
+  mutations: {},
+  modules: {},
+})
+
+
+export default store;
